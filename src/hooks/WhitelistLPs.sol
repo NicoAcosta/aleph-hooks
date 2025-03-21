77c15c19deb44e7ddbb0952ed8845c88eb0f034c
nicoacosta.eth <nicoacosta8@gmail.com>
2025-03-20 22:22:16 -0300

    feat(examples): whitelist lps

---
+++ src/hooks/WhitelistLPs.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {BaseHook} from "@uniswap/v4-periphery/src/utils/BaseHook.sol";
+import {IPoolManager} from "@uniswap/v4-core/src/interfaces/IPoolManager.sol";
+import {Hooks} from "@uniswap/v4-core/src/libraries/Hooks.sol";
+
+import {PoolKey} from "@uniswap/v4-core/src/types/PoolKey.sol";
+
+interface IWhitelist {
+    function isWhitelisted(address LP) external view returns (bool whitelisted);
+}
+
+contract WhitelistLPs is BaseHook {
+    IWhitelist public immutable whitelist;
+
+    error NotWhitelisted();
+
+    constructor(
+        IPoolManager _manager,
+        IWhitelist _whitelist
+    ) BaseHook(_manager) {
+        whitelist = _whitelist;
+    }
+
+    function getHookPermissions()
+        public
+        pure
+        override
+        returns (Hooks.Permissions memory)
+    {
+        return
+            Hooks.Permissions({
+                beforeInitialize: false,
+                afterInitialize: false,
+                beforeAddLiquidity: true,
+                afterAddLiquidity: false,
+                beforeRemoveLiquidity: false,
+                afterRemoveLiquidity: false,
+                beforeSwap: false,
+                afterSwap: false,
+                beforeDonate: false,
+                afterDonate: false,
+                beforeSwapReturnDelta: false,
+                afterSwapReturnDelta: false,
+                afterAddLiquidityReturnDelta: false,
+                afterRemoveLiquidityReturnDelta: false
+            });
+    }
+
+    function _beforeAddLiquidity(
+        address sender,
+        PoolKey calldata,
+        IPoolManager.ModifyLiquidityParams calldata,
+        bytes calldata
+    ) internal view override returns (bytes4) {
+        if (!whitelist.isWhitelisted(sender)) revert NotWhitelisted();
+
+        return this.beforeAddLiquidity.selector;
+    }
+}
